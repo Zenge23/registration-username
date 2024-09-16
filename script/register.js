db9902015f10e8b67fa91e0c11ce6e71fe5ac806
marietta zenge <mharietarh@gmail.com>
2024-09-16 11:39:43 +0000

    started new code

---
+++ script/register.js
@@ -0,0 +1,13 @@
+// Select form by id
+const registerForm = document.getElementById('register-form');
+function registeruser() {
+    // 
+}
+// Handle form submit event
+registerForm.onsubmit = function() {
+    // colldect username
+    //  collect email
+    // collect password
+    // save user information
+    // send confirmation email
+}
